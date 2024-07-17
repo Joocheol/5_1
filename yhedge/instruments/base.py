5416cbd03a5b0da380c70799fea6565a61d50508
Joocheol <joocheol@yonsei.ac.kr>
2024-07-17 11:50:02 +0900

    ok

---
+++ yhedge/instruments/base.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+
+import torch
+from torch import Tensor
+
+class BaseInstruments(ABC):
+
+    cost: float
+
+    @property
+    @abstractmethod
+    def spot(self) -> Tensor:
+        """"""
+
+    @property
+    @abstractmethod
+    def simulate(self, n_paths: int, time_horizon: float, **kwargs) -> None:
+        """"""
+
+    
+    
+
+    
